cb0e83e95b445f9d8278e23c69098d133739c17f
Hayden G <hayden.garland@gmail.com>
2024-04-29 21:29:52 -0500

    more changes to script

--- dwm-install.sh
+++ dwm-install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# dependencies
 echo "Installing dependencies"
-sudo pacman -Sy xorg-server xorg-xinit libx11 libxinerama libxft webkit2gtk gd font-symbola libXft-bgra picom alacritty firefox
+sudo pacman -Sy xorg-server xorg-xinit libx11 libxinerama libxft webkit2gtk gd font-symbola libXft-bgra picom alacritty firefox ttf-jetbrains-mono nnn
 
-echo "Creating and clone directories"
+# base configurations
+echo "Creating directories"
 mkdir /home/hayden/.config
 mkdir /home/hayden/.local/bin
 
+echo "Cloning suckless applications"
+cd /home/hayden/.config/
 for app in dwm dmenu slstatus st; do
 	git clone "https://git.suckless.org/$app"
 	cd "$app"
@@ -14,3 +18,6 @@ for app in dwm dmenu slstatus st; do
 	sudo make clean install
 	cd ..
 done
+
+# startup
+cp /etc/X11/xinit/xinitrc /home/hayden/.xinitrc
